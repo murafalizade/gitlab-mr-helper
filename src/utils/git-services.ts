9d4bcc906f09dc7e83bb1b203d9b1bb54bf31b54
muradeliyev <muradaliyev2229@gmail.com>
2025-07-25 21:57:06 +0400

    refactor: gitlabservice

--- src/utils/git-services.ts
+++ src/utils/git-services.ts
@@ -1,26 +1,46 @@
 import axios from 'axios';
-import {loadConfig} from "../libs/config.ts";
+import {GitlabMRConfig, loadConfig} from "../libs/config.ts";
 import {StateOptionType} from "../types/state-option.type.ts";
 
 export class GitServices {
-     config = loadConfig();
+     config:GitlabMRConfig;
+     headers = {};
+
+     constructor() {
+         this.config = loadConfig();
+         if(!this.config.apiToken || !this.config.projects){
+             throw new Error('No projects or API token configured');
+         }
+
+        this.headers = {
+            'PRIVATE-TOKEN': this.config.apiToken,
+        };
+     }
+
+     fetchUser = async (): Promise<any> => {
+         const {  apiToken, apiUrl } = this.config;
+
+         const userRes = await axios.get(`${apiUrl}/api/v4/user`, { headers:this.headers });
+         return userRes.data;
+     }
 
     fetchMergeRequests = async (state: StateOptionType = StateOptionType.ALL): Promise<any> => {
-        const { projects, apiToken, apiUrl } = this.config;
+        const { projects, apiUrl } = this.config;
+        let allMergeRequests = [];
+        const user = await this.fetchUser();
 
-        if (!projects || !apiToken) {
-            throw new Error('No projects or API token configured');
+        if(!user) {
+            console.log('No Merge Requests Found');
+            return;
         }
 
-        const headers = {
-            'PRIVATE-TOKEN': apiToken
-        };
-        let allMergeRequests = [];
         for (const project of Object.keys(projects)) {
             const url = `${apiUrl}/api/v4/projects/${encodeURIComponent(project)}/merge_requests`;
-            const params = state === StateOptionType.ALL ? {} : { state };
-
-            const response = await axios.get<any[]>(url, { headers, params });
+            const params =
+                state === StateOptionType.ALL
+                    ? { author_id: user?.id } // still limit to assigned MRs
+                    : { state, author_id: user?.id };
+            const response = await axios.get<any[]>(url, { headers:this.headers, params });
             allMergeRequests.push(...response.data);
         }
 
@@ -28,11 +48,10 @@ export class GitServices {
     }
 
     fetchMergeCommits = async (mr: any): Promise<any[]> => {
-        const { apiToken, apiUrl} = this.config;
-        const headers = { 'PRIVATE-TOKEN': apiToken };
+        const {apiUrl} = this.config;
 
         const url = `${apiUrl}/api/v4/projects/${encodeURIComponent(mr.project_id)}/merge_requests/${mr.iid}/commits`;
-        const res = await axios.get<any[]>(url, { headers });
+        const res = await axios.get<any[]>(url, { headers:this.headers });
         return res.data;
     };
 }
